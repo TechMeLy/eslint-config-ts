bc289ba1ecfa53024c5d603879f847d254b473a0
Harry Tran <nhattq.coding@gmail.com>
2021-09-08 20:09:13 +0700

    fix(vue): update vue config

--- kimochi/vue/index.js
+++ kimochi/vue/index.js
@@ -1,13 +1,4 @@
 module.exports = {
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      tsx: true,
-    },
-  },
   extends: ['@techmely/eslint-config-ts', 'plugin:vue/vue3-recommended'],
   rules: {
     'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
