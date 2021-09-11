39066b61fed08b0fb361e96f6636006882c9cd6d
Harry Tran <nhattq.coding@gmail.com>
2021-09-11 12:44:18 +0700

    feat(vue): update eslint vue project

--- kimochi/vue/index.js
+++ kimochi/vue/index.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: ['@techmely/eslint-config-ts', 'plugin:vue/vue3-recommended'],
   rules: {
+    // Ignore specific value in vue 3 setup script
+    'import/no-unresolved': ['off'],
+    'import/extensions': ['off'],
+    '@typescript-eslint/no-unused-vars': ['off'],
+    'no-undef': ['off'],
+
     'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
     'vue/html-self-closing': 'off',
     'vue/no-v-html': 'off',
